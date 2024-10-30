cd452d4a6bdb5432efa8e7b336caa8019173b6e1
sorae-9 <jisongkonghui@gmail.com>
2024-10-30 22:51:29 +0900

    登録後、更新後メッセージ表示修正

--- exerciseHabitsRecord/views.py
+++ exerciseHabitsRecord/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
-from .models import Exercise
-from .forms import ExerciseGoalForm, ExerciseResultForm
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from .models import Exercise
+from .forms import ExerciseGoalForm, ExerciseResultForm
 
 #　登録完了後の画面
 def create_done(request):
@@ -12,15 +13,22 @@ def create_done(request):
 
 # 運動目標登録画面のView
 class ExerciseGoalView(LoginRequiredMixin, CreateView):
+  login_url = reverse_lazy("exerciseHabitsRecord:login")
   model = Exercise
   form_class = ExerciseGoalForm
   template_name = "exercise_habits/goal_form.html"
   # 登録完了後の遷移先
   success_url = reverse_lazy("exerciseHabitsRecord:create_done")
+  
   def get_context_data(self, **kwargs):
     context = super(ExerciseGoalView, self).get_context_data(**kwargs)
-    context['message_type'] = "create_goal"
+    context["message_type"] = "create_goal"
     return context
+  
+  def form_valid(self, form):
+    messages.success(self.request, "運動目標の登録が完了しました。")
+    return super().form_valid(form)
+  
   def post(self, request, *args, **kwargs):
     user = request.user
     data = request.POST.dict()
@@ -33,15 +41,22 @@ class ExerciseGoalView(LoginRequiredMixin, CreateView):
 
 # 運動実績登録画面のView
 class ExerciseResultView(LoginRequiredMixin, CreateView):
+  login_url = reverse_lazy("exerciseHabitsRecord:login")
   model = Exercise
   form_class = ExerciseResultForm
   template_name = "exercise_habits/result_form.html"
   # 登録完了後の遷移先
   success_url = reverse_lazy("exerciseHabitsRecord:create_done")
+
   def get_context_data(self, **kwargs):
     context = super(ExerciseResultView, self).get_context_data(**kwargs)
-    context['message_type'] = "create_result"
+    context["message_type"] = "create_result"
     return context
+  
+  def form_valid(self, form):
+    messages.success(self.request, "運動実績の登録が完了しました。")
+    return super().form_valid(form)
+  
   def post(self, request, *args, **kwargs):
     user = request.user
     data = request.POST.dict()
@@ -51,19 +66,6 @@ class ExerciseResultView(LoginRequiredMixin, CreateView):
       return self.form_valid(form)
     else:
       return self.form_invalid(form)
-
-# 運動記録一覧画面のView
-class ExerciseHabitsList(LoginRequiredMixin, ListView):
-  login_url = reverse_lazy("login")
-  model = Exercise
-  template_name = "exercise_habits/list_form.html"
-  def get_context_data(self, **kwargs):
-    context = super(ExerciseHabitsList, self).get_context_data(**kwargs)
-    if self.request.user.is_authenticated:
-      context["exercise_list"] = Exercise.objects.filter(user_id=self.request.user)
-    else:
-      context["exercise_list"] = None
-    return context
 
 # ログイン画面
 class LoginView(LoginView):
--- staffroom/views.py
+++ staffroom/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from exerciseHabitsRecord.models import Exercise
 from exerciseHabitsRecord.forms import ExerciseGoalForm, ExerciseResultForm
 
@@ -14,7 +15,8 @@ def delete_done(request):
   return render(request, "delete_done.html")
 
 # 運動記録一覧画面のView
-class ExerciseHabitsList(ListView):
+class ExerciseHabitsList(LoginRequiredMixin, ListView):
+  login_url = reverse_lazy("exerciseHabitsRecord:login")
   model = Exercise
   template_name = "exercise_habits/list_form.html"
   def get_context_data(self, **kwargs):
@@ -27,30 +29,42 @@ class ExerciseHabitsList(ListView):
 
 # 運動目標更新画面のView
 class ExerciseGoalUpdateView(LoginRequiredMixin, UpdateView):
+  login_url = reverse_lazy("exerciseHabitsRecord:login")
   model = Exercise
   form_class = ExerciseGoalForm
   template_name = "exercise_habits/goal_form.html"
-  # 登録完了後の遷移先
+  # 更新完了後の遷移先
   success_url = reverse_lazy("staffroom:exerciseHabitsRecord:update_done")
   def get_context_data(self, **kwargs):
     context = super(ExerciseGoalUpdateView, self).get_context_data(**kwargs)
-    context['message_type'] = "update_goal"
+    context["message_type"] = "update_goal"
     return context
+  
+  def form_valid(self, form):
+    messages.success(self.request, "運動目標の更新が完了しました。")
+    return super().form_valid(form)
+
 
 # 運動実績更新画面のView
 class ExerciseResultUpdateView(LoginRequiredMixin, UpdateView):
+  login_url = reverse_lazy("exerciseHabitsRecord:login")
   model = Exercise
   form_class = ExerciseResultForm
   template_name = "exercise_habits/result_form.html"
-  # 登録完了後の遷移先
+  # 更新完了後の遷移先
   success_url = reverse_lazy("staffroom:exerciseHabitsRecord:update_done")
   def get_context_data(self, **kwargs):
     context = super(ExerciseResultUpdateView, self).get_context_data(**kwargs)
     context['message_type'] = "update_result"
     return context
+  
+  def form_valid(self, form):
+    messages.success(self.request, "運動実績の更新が完了しました。")
+    return super().form_valid(form)
 
 # 運動情報削除画面のView
 class ExerciseDeleteView(LoginRequiredMixin, DeleteView):
+  login_url = reverse_lazy("exerciseHabitsRecord:login")
   model = Exercise
   template_name = "delete_form.html"
   # 削除完了後の遷移先
